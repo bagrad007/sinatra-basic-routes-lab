517c0bb2ad962622526a9acd8d15c88fbd6d02c6
Luke Gill <bagrad007@gmail.com>
2021-02-19 12:13:37 -0500

    commit tree d0f05340233508d64684a5586c2f8f189b215acd

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
-require_relative 'config/environment'
+require "pry"
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    # binding.pry
+    "My name is "
+  end
+  get "/hometown" do
+    # binding.pry
+    "My hometown is "
+  end
+  get "/:favorite-song" do
+    # binding.pry
+    "My favorite song is "
+  end
 end
